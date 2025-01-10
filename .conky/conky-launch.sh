2d09242a510efe44613c467b3560c881f851fa37
Nandor206 <nandornotfound@gmail.com>
2025-01-10 18:02:02 +0100

    Initial commit

---
+++ .conky/conky-launch.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+killall conky
+# start for conky-calendar
+#conky -c ~/.conky/conf/conky-calendar/conky-calendar &
+# start for conky-clock
+conky -c ~/.conky/conf/conky-clock/conky-clock &
+# start for conky-computer-metrics
+conky -c ~/.conky/conf/conky-computer-metrics/conky-computer-metrics &
+# start for conky-exploded-view
+#conky -c ~/.conky/conf/conky-exploded-view/conky-exploded-view &
+# start for conky-rss-reader
+#conky -c ~/.conky/conf/conky-rss-reader/conky-rss-reader &
+# start for conky-weather
+#conky -c ~/.conky/conf/conky-weather/conky-weather &
